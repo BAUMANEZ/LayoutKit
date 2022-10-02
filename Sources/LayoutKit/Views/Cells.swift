0b45689d0aae2f51690809fa6ebb60123f669b19
Arseny <62566543+BAUMANEZ@users.noreply.github.com>
2022-10-02 21:45:25 +0300

    Update Cells.swift

--- Sources/LayoutKit/Views/Cells.swift
+++ Sources/LayoutKit/Views/Cells.swift
@@ -32,12 +32,11 @@ open class Cell: UIView, Compositional {
         return .zero
     }
     
-    public init() {
+    public required override init(frame: CGRect = .zero) {
         super.init(frame: .zero)
         setupContent()
         setup()
     }
-    
     public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
